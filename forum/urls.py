f9522e18bb57b4779dc25f77aa96ccb4b2ffaedb
Namul Amin <nazmulamin2016@gmail.com>
2025-04-23 00:43:20 +0600

    Resolved merge conflicts

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for forum project.
+URL configuration for fourm project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,15 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
-from forumapp import views  # Import views to use the user_profile view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Add a URL pattern for the user profile that expects a username.
-    path('profile/<str:username>/', views.user_profile, name='user_profile'),
-    # Include all other URLs from the forumapp.
-    path('', include("forumapp.urls")),
+    path('', include("forumapp.urls"))
 ]
