303fed25e5433d38832be2ab0b478fcd7a958de4
iamrejwan <codex11099@gmail.com>
2025-04-23 22:50:37 +0600

    updated

--- forumapp/forms.py
+++ forumapp/forms.py
@@ -9,6 +9,13 @@ class PostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
 
+        # Add help_text to fields
+        self.fields['title'].help_text = 'Keep it short and descriptive.'
+        self.fields['category'].help_text = 'Choose the most relevant category.'
+        self.fields['tags'].help_text = 'Separate tags with commas.'
+        self.fields['content'].help_text = 'Be detailed, but concise.'
+    
+
         # Add Tailwind classes to form fields
         self.fields['title'].widget.attrs.update({
             'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
@@ -25,4 +32,10 @@ class PostForm(forms.ModelForm):
             'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
             'rows': '12',
             'placeholder': 'Write your post content here...'
-        })
+        
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 10:
+            raise forms.ValidationError("The title must be at least 10 characters long.")
+        return title
+})
